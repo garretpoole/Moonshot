40d6bed3ee22f9992fe9f3df4b4a992f3a1f3ef8
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-15 00:50:49 -0700

    ScrollView for vertical and horizontal scrolling data

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,18 +7,35 @@
 
 import SwiftUI
 
+struct CustomText: View{
+    let text: String
+    
+    var body: some View{
+        Text(text)
+    }
+    
+    init(_ text: String){
+        print("Creating new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader{ geo in
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
+        //can use .horizontal in call with HStack
+        ScrollView(.vertical){
+            //.infinity to scroll entire screen
+            //VStack all rows are created immediately, not as they come into view
+            //Lazy loads them on demand but automatically take up all availbale free space in layout
+            //Regular only take up the space needed for their children
+            LazyVStack(spacing: 10){
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
-        
-            
-            
     }
 }
 
