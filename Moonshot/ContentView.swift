f144f2d98d7e227ec4daede6d7600c02df411923
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-18 10:11:29 -0700

    basic view to show missions

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -11,8 +11,39 @@ struct ContentView: View {
     //extension to make this; generic decode to now load any json
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label: {
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -13,9 +13,16 @@ struct Mission: Codable, Identifiable {
         let name: String
         let role: String
     }
-    
     let id: Int
     let launchDate: String?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String{
+        "Apollo \(id)"
+    }
+    
+    var image: String{
+        "apollo\(id)"
+    }
 }
