549e8d626b5749d139614c2ce81b1c614ee2dabe
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-19 02:02:22 -0700

    added AstronautView, accessed from MissionView

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Garret Poole on 3/19/22.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    var body: some View {
+        ScrollView{
+            VStack{
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        //force unwrap, must be careful when doing so
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
